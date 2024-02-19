bbd8a6835f770fe97b2a389a71819e32e7e5ba8a
salvoPernicano <salvo.pernicano@gmail.com>
2024-02-19 13:21:01 +0100

    feat struttura header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 
+export default{
+    components : {
+        AppHeader
+    }
+}
 </script>
 
 <template>
-
+    <AppHeader/>
 </template>
 
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,37 @@
 <template>
     <header>
-        <div>
+        <div class="socialLogin container">
             <div>
-
-            </div>
-        </div>
-        <div>
-            <div>
-
-            </div>
-        </div>
-        <div>
-            <nav>
-                <ul>
+                <span> <i class="fa-regular fa-envelope"></i> Superhit Top Movie <span>** King Star **</span></span>
+                <ul class="socialMenu">
 
                 </ul>
+            </div>
+            <div>
+                <span> <i class="fa-solid fa-lock"></i> Log In</span>
+            </div>
+        </div>
+        <div class="wrapper ">
+            <div class="logoSearch container">
+
+                <figure>
+                    <img src="../assets/img/cropped-logo.png" alt="">
+                </figure>
+                <div class="search-container">
+                    <form>
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="button">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="bottomNav container">
+            <nav>
+                <ul class="navMenu">
+                    <li v-for="element in navArray"><a href="#">{{ element.name }}</a></li>
+                </ul>
                 <span>
-                    
+                    <i class="fa-solid fa-basket-shopping"></i>
                 </span>
             </nav>
         </div>
@@ -26,14 +41,76 @@
 
 <script>
 export default {
-    data () {
-        
+    name: 'AppHeader',
+    data() {
+        return {
+            navArray: [
+                {
+                    name: 'Home',
+                    subNames: 'Home'
+                },
+                {
+                    name: 'Celebritiy',
+                    subNames: 'Movie'
+                },
+                {
+                    name: 'Movie',
+                    subNames: 'Home'
+                },
+                {
+                    name: 'Page',
+                    subNames: 'Home'
+                },
+                {
+                    name: 'Shop',
+                    subNames: 'Home'
+                },
+                {
+                    name: 'Blog',
+                    subNames: 'Home'
+                },
+                {
+                    name: 'Contact Us',
+                    subNames: 'Home'
+                }
 
-        return {}
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss" scoped>
+@use '../scss/style.scss' as *;
+@use '../scss/partials/variables' as*;
+
+nav,
+.navMenu,
+.socialLogin,
+.logoSearch,
+.bottomNav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1em;
+    gap: 8px;
+}
+
+.wrapper{
+    border-block: 1px solid grey;
+}
+
+nav{
+    width: 100%;
+}
+
+.navMenu li{
+    list-style: none;
+    a{
+        text-decoration: none;
+        color: $appWhite;
+    }
+}
+
 
 </style>
